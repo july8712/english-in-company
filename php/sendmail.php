b4f7c7ff2cc7698422b646068b0e4fbcecdf9f46
Mariano Valoira <mavt_hmb@yahoo.com.ar>
2022-01-02 16:44:03 -0300

    * ajuste formulario

--- php/sendmail.php
+++ php/sendmail.php
@@ -41,10 +41,10 @@
                         //Server settings
                         //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
                         $mail->isSMTP();                                            //Send using SMTP
-                        $mail->Host       = '';                     //Set the SMTP server to send through
+                        $mail->Host       = 'c2460306.ferozo.com';                     //Set the SMTP server to send through
                         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                        $mail->Username   = '';                     //SMTP username
-                        $mail->Password   = '';                               //SMTP password
+                        $mail->Username   = 'no-reply@c2460306.ferozo.com';                     //SMTP username
+                        $mail->Password   = 'mIKW@PcO3C';                               //SMTP password
                         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
                         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
